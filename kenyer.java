0a5888bf850239931fd2afe00a450f3801f1a93b
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-03-21 14:13:30 +0100

    házibefejezni

---
+++ kenyer.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package feb;
+
+/**
+ *
+ * @author User
+ */
+public class kenyer {
+    public String name;
+    private String tipus;
+    private String szin;
+    private Integer ar;
+    private Integer db;
+    private Integer suly;
+
+    public Integer getAr() {
+        return ar;
+    }
+
+    public void setAr(Integer ar) {
+        this.ar = ar;
+    }
+
+    public Integer getDb() {
+        return db;
+    }
+
+    public void setDb(Integer db) {
+        this.db = db;
+    }
+
+    public Integer getSuly() {
+        return suly;
+    }
+
+    public void setSuly(Integer suly) {
+        this.suly = suly;
+    }
+    
+    
+}
